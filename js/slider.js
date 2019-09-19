93d407b4cce9c78a288836b31c3977bb31a2780b
Vũ Đức Huy Hoàng <dev.vuhoang1802@gmail.com>
2019-09-19 22:58:48 +0700

    nháp 3
    ngày 19/9

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,29 @@
-alert('haong');
+
+$(document).ready(function(){
+
+	$('.banner').slick({
+	  dots: false,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  autoplay:true,
+	  autoplaySpeed: 2000
+
+	});
+	$('.feed').slick({
+	  dots: false,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  autoplay:true,
+	  autoplaySpeed: 4000,
+	  arrows: true,
+	  prevArrow: '<div class="slick-prev"><</div>',
+	  nextArrow: '<div class="slick-next">></div>'
+
+	});
+});
+
+
